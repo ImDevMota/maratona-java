a07b5103dafebf7c3137213415a9fa3592ee6b65
Thiago Mota <thiago.you23@gmail.com>
2025-08-21 23:56:11 -0300

    Exercícios modificador de acesso

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/PessoaTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("Naruto");
+
+        pessoa.setIdade(11);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
